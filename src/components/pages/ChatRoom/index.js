6a7eadaada6dc73a495ec19682d8979131f03176
AnguleMathias <angulemathias3@gmail.com>
2020-04-24 17:22:02 +0300

    structure chat room

--- src/components/pages/ChatRoom/index.js
+++ src/components/pages/ChatRoom/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const ChatRoom = () => <div>chat room content here</div>;
+import ChatNavbar from './ChatNavbar';
+import MessageInput from './MessageInput';
+import MessagesList from './MessagesList';
+
+const Container = styled.div`
+  background: url(/assets/chat-background.jpg);
+  display: flex;
+  flex-flow: column;
+  height: 100vh;
+`;
+
+const ChatRoomMessages = () => {
+  return (
+    <Container>
+      <ChatNavbar chat={chat} history={history} />
+      <MessagesList />
+      <MessageInput />
+    </Container>
+  );
+};
+
+const ChatRoom = () => {
+  return <ChatRoomMessages />;
+};
 
 export default ChatRoom;
