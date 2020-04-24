cc5e9aee53aa5c29042492fe093e5cd38ba7dc3a
eshaamrabaney <eshaam.rabaney@gmail.com>
2020-04-24 20:04:37 +0200

    Integrate with Bootstrap and Reactstrap

--- src/components/pages/ChatRoom/index.js
+++ src/components/pages/ChatRoom/index.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
+import React , {Component}from 'react';
+import DefaultLayout from '../../molecules/Layout/DefaultLayout';
 
 import ChatNavbar from '../../molecules/ChatNavbar';
 import MessageInput from '../../molecules/MessageInput';
 import MessagesList from '../../organisms/MessagesList';
 
-const Container = styled.div`
-  background: url(/assets/chat-background.jpg);
-  display: flex;
-  flex-flow: column;
-  height: 100vh;
-`;
+class ChatRoom extends Component {
 
-const ChatRoomMessages = () => {
-  return (
-    <Container>
-      <ChatNavbar />
-      <MessagesList />
-      <MessageInput />
-    </Container>
-  );
-};
-
-const ChatRoom = () => {
-  return <ChatRoomMessages />;
-};
+  render() {
+    return (
+      <DefaultLayout>
+        <ChatNavbar/>
+        <MessagesList/>
+        <MessageInput/>
+      </DefaultLayout>
+    );
+  }
+}
 
 export default ChatRoom;
--- src/components/pages/ChatsScreen/index.js
+++ src/components/pages/ChatsScreen/index.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, {Fragment} from 'react';
 
-import NavBar from '../../molecules/NavBar';
 import NewChatButton from '../../molecules/NewChatButton';
 import ChatListItems from '../../organisms/ChatListItems'
 
-const Main = styled.div`
-  height: 100vh;
-`;
+import {Row, Container, Col} from 'reactstrap';
+import DefaultLayout from '../../molecules/Layout/DefaultLayout';
 
 const ChatsScreen = () => (
-  <Main>
-    <NavBar />
-    <ChatListItems />
-    <NewChatButton />
-  </Main>
+
+  <DefaultLayout>
+    <ChatListItems/>
+  </DefaultLayout>
+
 );
 
 export default ChatsScreen;
